0e3b4b39873d18638a6452f06b83c44ea4cc5586
nilamtambe <111958530+nilamtambe@users.noreply.github.com>
2022-12-10 13:35:18 +0530

    Add files via upload

--- src/helpers/MenuList.js
+++ src/helpers/MenuList.js
@@ -1,1 +1,39 @@
+import cake1 from "../assets/cake1.jpg";
+import cake11 from "../assets/cake11.jpg";
+import cake3 from "../assets/cake3.jpg";
+import cake9 from "../assets/cake9.jpg";
+import cake5 from "../assets/cake5.jpg";
+import cake6 from "../assets/cake6.jpg";
 
+export const MenuList = [
+  {
+    name: "Choclate Cake",
+    image: cake1,
+    price: 199,
+  },
+  {
+    name: "Vanilla Cake",
+    image: cake11,
+    price: 149,
+  },
+  {
+    name: "Strawberi Cake",
+    image: cake3,
+    price: 200.53,
+  },
+  {
+    name: "Ice Cake",
+    image: cake9,
+    price: 174.99,
+  },
+  {
+    name: "Mango Cake",
+    image: cake5,
+    price: 299,
+  },
+  {
+    name: "Mix Flavour Cake",
+    image: cake6,
+    price: 1997.99,
+  },
+];
